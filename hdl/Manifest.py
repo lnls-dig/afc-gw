1b5948dca2368db625a70811c08f99913be29795
Lucas Russo <lerwys@gmail.com>
2020-01-07 17:17:13 -0200

    Manifest.py: move more specific Manifest to inside

--- hdl/Manifest.py
+++ hdl/Manifest.py
@@ -1,5 +1,7 @@
-modules = { "local": [
-                "ip_cores/general-cores",
-                "ip_cores/infra-cores",
-            ]
-    };
+modules = { "local" : [
+    "rtl",
+    "modules",
+] }
+
+if action == "synthesis":
+    modules["local"].append("syn/common")
