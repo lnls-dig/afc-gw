7a7c1ed181ab3a9bbc8144e08efa27f6a415af64
Lucas Russo <lerwys@gmail.com>
2020-01-23 11:29:32 -0200

    rtl: move afc_base to base directory

--- hdl/rtl/Manifest.py
+++ hdl/rtl/Manifest.py
@@ -1,9 +1,3 @@
-files = [
-    "clk_gen_mgt.vhd",
-    "clk_gen.vhd",
-    "sys_pll.vhd",
-    "afc_base_regs.vhd",
-    "afc_base_regs_pkg.vhd",
-    "afc_base_pkg.vhd",
-    "afc_base.vhd"
-]
+modules = { "local" : [
+    "base",
+] }
